e0bd3052fb388bd8ec313ed5d9cb9e11633a215b
wskvfhprrk <75412985@qq.com>
2019-06-22 21:04:46 +0800

    自定义用户谁逻辑

---
+++ security-browser/src/main/java/com/hejz/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.hejz.security.browser;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @Auther: hejz
+ * @Description:
+ * @Date: 2019/6/22 19:58
+ */
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+//        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
---
+++ security-browser/src/main/java/com/hejz/security/browser/MyUserDatailsService.java
@@ -0,0 +1,33 @@
+package com.hejz.security.browser;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: hejz
+ * @Description:
+ * @Date: 2019/6/22 20:31
+ */
+@Component
+@Slf4j
+public class MyUserDatailsService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("登陆用户名：{}",username);
+        //根据用户名查找用户信息——从数据库中要读取出来的
+        String password = passwordEncoder.encode("123456");
+        log.info("密码为：{}",password);
+        return new User(username, password,true,true,true,true, AuthorityUtils.commaSeparatedStringToAuthorityList("ADMIN"));
+    }
+}
